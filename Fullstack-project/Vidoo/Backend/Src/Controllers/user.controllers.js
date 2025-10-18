61a26d26b549fb1b9086c89dcbe302c6d33df026
Raktimb98 <raktimb181@gmail.com>
2025-10-18 14:08:25 +0530

    Add getWatchHistory functionality: implement user watch history retrieval with video owner details

--- Fullstack-project/Vidoo/Backend/Src/Controllers/user.controllers.js
+++ Fullstack-project/Vidoo/Backend/Src/Controllers/user.controllers.js
@@ -8,6 +8,7 @@ import {
   deletePreviousImage,
   extractPublicIdFromUrl,
 } from '../Utils/deletePreviousImage.js';
+import mongoose from 'mongoose';
 
 const generateAccessTokenAndRefreshToken = async (userId) => {
   try {
@@ -366,6 +367,60 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, channel[0], 'User channel profile fetched'));
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: 'Video',
+        localField: 'watchHistory',
+        foreignField: '_id',
+        as: 'watchHistoryVideos',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'User',
+              localField: 'uploadedBy',
+              foreignField: '_id',
+              as: 'owner',
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0]?.watchHistoryVideos || [],
+        'User watch history fetched successfully'
+      )
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -377,4 +432,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory,
 };
