0e2c09709abc17cd19f0df87a3f9971d24539db1
Raktimb98 <raktimb181@gmail.com>
2025-10-19 13:09:21 +0530

    Refactor user routes: reorganize imports and add secured routes for account management and user profile retrieval

--- Fullstack-project/Vidoo/Backend/Src/Routes/user.routes.js
+++ Fullstack-project/Vidoo/Backend/Src/Routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import {
+  registerUser,
   loginUser,
   logoutUser,
-  registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from '../Controllers/user.controllers.js';
 import { upload } from '../Middlewares/multer.middlewares.js';
 import { verifyJWT } from '../Middlewares/auth.middlewares.js';
@@ -28,5 +35,11 @@ router.route('/login').post(loginUser);
 //Secured Routes
 router.route('/logout').post(verifyJWT, logoutUser);
 router.route('/refresh-token').post(refreshAccessToken);
-
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/current-user').get(verifyJWT, getCurrentUser);
+router.route('/update-account').patch(verifyJWT, updateAccountDetails);
+router.route('/update-avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
+router.route('/update-cover-image').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
+router.route('/c/:username').get(verifyJWT,getUserChannelProfile);
+router.route('/watch-history').get(verifyJWT, getWatchHistory);
 export default router;
