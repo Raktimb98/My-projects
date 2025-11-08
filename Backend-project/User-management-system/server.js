1229f5d2715ff16a60efc98111459139dfcb9194
Raktimb98 <raktimb181@gmail.com>
2025-11-08 14:35:56 +0530

    Establish database connection in server initialization

--- Backend-project/User-management-system/server.js
+++ Backend-project/User-management-system/server.js
@@ -2,10 +2,12 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import app from './src/app.js';
+import connectDB from './src/config/database.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
