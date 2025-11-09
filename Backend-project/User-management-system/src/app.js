d76044566d8948d2841324eea0790f0220268cff
Raktimb98 <raktimb181@gmail.com>
2025-11-09 18:09:19 +0530

    Refactor app.js to improve express middleware configuration and enhance route organization

--- Backend-project/User-management-system/src/app.js
+++ Backend-project/User-management-system/src/app.js
@@ -3,13 +3,17 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true ,limit: '10kb'}));
 app.use(cors());
 
+// Basic routes
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API running!' });
+});
+app.get('/about', (req, res) => {
+  res.send('This is THE about page!');
 });
 
 export default app;
